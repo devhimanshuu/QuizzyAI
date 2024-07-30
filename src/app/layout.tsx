e1c70956f30b6166da24d071370d819613c4f748
devhimanshuu <devhimanshuu@gmail.com>
2024-07-31 01:56:32 +0530

    app

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Quizzy.ai",
+  description: "Make quizzes with AI in seconds",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
+        {children}
+      </body>
+    </html>
+  );
+}
